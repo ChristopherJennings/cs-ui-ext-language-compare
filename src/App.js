192dfc37761a2044ee9f474e9a03f83ead2402a4
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2021-11-30 11:58:12 -0500

    Updates to pull versions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import ContentstackUIExtension from '@contentstack/ui-extensions-sdk';
 
-import { Button, Select } from '@contentstack/venus-components';
+import { Button, InstructionText } from '@contentstack/venus-components';
 
 function App() {
   const [error, setError] = useState(null);
   const [extension, setExtension] = useState(null);
+  const [locales, setLocales] = useState([]);
+  const [entry, setEntry] = useState(null);
 
   useEffect(() => {
     // eslint-disable-next-line no-restricted-globals
@@ -19,41 +21,63 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    if (extension) {
+
+      setEntry(extension.entry.getData());
+
+      extension.stack.getLocales().then(locales => {
+        setLocales(locales.locales)
+      })
+    }
+  }, [extension])
+
   if (error) {
     return <p>Error: {error.message}</p>
   }
 
+  if (!extension) {
+    return <p>Loading...</p>
+  }
+
+  const getTranslations = (fieldUid) => {
+    if (extension) {
+      console.log('getTranslations', extension);
+      //locales[0].code
+      const api_key="blt02b0f4733a07e2cf"
+      const token="csd577949ac8927da28f0d3304"
+      extension.stack.ContentType(extension.entry.content_type.uid).Entry(entry.uid).language("fr-fr").fetch().then(entry => {
+        console.log(entry)
+      })
+    }
+
+    return null
+  }
+
   return (
     <div>
+      <InstructionText>
+        Select the locales to see
+      </InstructionText>
       <Button>Hello World</Button>
 
-      <Select
-        selectLabel="Select a field"
-        onChange={e => {
-          console.log(e);
-        }}
-        options={[
-          {
-            id: 0,
-            label: 'Option 1',
-            searchableLabel: 'ssssOption 1',
-            value: 1
-          },
-          {
-            id: 1,
-            label: 'Option 2',
-            searchableLabel: 'ssssOption 2',
-            value: 2
-          },
-          {
-            id: 2,
-            label: 'Option 3',
-            searchableLabel: 'ssssOption 3',
-            value: 3
-          }
-        ]}
-        value={null}
-      />
+      <div>
+        Locale: {extension.entry.locale}
+      </div>
+      <div>
+        Locales: {locales?.map(locale => locale.name + "("+locale.code+")").join(', ')}
+      </div>
+
+      {extension.entry
+        ? extension.entry.content_type.schema.map(field => {
+          return <div key={field.uid}>
+            {field.display_name}
+            <Button onClick={() => getTranslations(field.uid)}>See translations</Button>
+          </div>
+        })
+        : "No Entry"
+      }
+
     </div>
   );
 }
